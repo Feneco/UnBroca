1b8d4be8eab7f6e6816a2c7cbff8f1f0dfc935a6
Wagner <wagnerweb2010@gmail.com>
2023-06-26 17:49:43 -0300

    feat: added basic drawer navigation

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import "react-native-gesture-handler";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { getHeaderTitle } from "@react-navigation/elements";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -9,43 +11,56 @@ import { CustomNavigationHeader } from "./ui/components";
 import { ColorPalette } from "./constants";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function Main() {
+  return (
+    <Stack.Navigator
+      initialRouteName="main"
+      screenOptions={{
+        header: ({ navigation, route, options, back }) => {
+          const title = getHeaderTitle(options, route.name);
+
+          return (
+            <CustomNavigationHeader
+              title={title}
+              onPress={navigation.goBack}
+              back={back}
+            />
+          );
+        },
+      }}
+    >
+      <Stack.Screen
+        name="main"
+        component={MainShopperPage}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="demo"
+        component={DemoPage}
+        options={{ headerTitle: "DEMO PAGE!" }}
+      />
+      <Stack.Screen
+        name="productPage"
+        component={ProductPage}
+        options={{ headerTitle: "Página da marmita" }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <SafeAreaView style={{ backgroundColor: ColorPalette.Gray1, flex: 1 }}>
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="main"
-          screenOptions={{
-            header: ({ navigation, route, options, back }) => {
-              const title = getHeaderTitle(options, route.name);
-
-              return (
-                <CustomNavigationHeader
-                  title={title}
-                  onPress={navigation.goBack}
-                  back={back}
-                />
-              );
-            },
-          }}
-        >
-          <Stack.Screen
-            name="main"
-            component={MainShopperPage}
-            options={{ headerShown: false }}
+        <Drawer.Navigator initialRouteName="app">
+          <Drawer.Screen
+            name="app"
+            component={Main}
+            options={{ headerShown: false, headerTitle: "Página inicial" }}
           />
-          <Stack.Screen
-            name="demo"
-            component={DemoPage}
-            options={{ headerTitle: "DEMO PAGE!" }}
-          />
-          <Stack.Screen
-            name="productPage"
-            component={ProductPage}
-            options={{ headerTitle: "Página da marmita" }}
-          />
-        </Stack.Navigator>
+        </Drawer.Navigator>
       </NavigationContainer>
       <StatusBar style="light" backgroundColor={ColorPalette.Primary} />
     </SafeAreaView>
